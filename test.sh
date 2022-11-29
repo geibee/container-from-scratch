2332bec0db97d5ea9e24156d61110a6d840ca5fa
regmarmcem <tknktknktknktknktknk@gmail.com>
2022-11-29 11:43:58 +0900

    [fix] test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,11 @@
+rm tmp/exec.fifo
+if ls rootfs > /dev/null 2>&1; then
+    echo "rootfs" exists
+else
+    docker run --rm -d --name ubuntu ubuntu:18.04 tail -f /dev/null
+    docker export ubuntu > rootfs.tar
+    tar xf rootfs.tar -C rootfs
+    rm rootfs.tar
+fi
 go build -o cfs 
 sudo ./cfs run --command "/bin/echo hello"
