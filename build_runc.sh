942435d1a88af380dc9db8a368d4a82073cd6daf
regmarmcem <tknktknktknktknktknk@gmail.com>
2022-12-23 00:13:53 +0900

    [fix] runc as debug mode in build_runc.sh

--- build_runc.sh
+++ build_runc.sh
@@ -20,4 +20,4 @@ sudo ./runc spec -b runc_workspace/runc/bundle
 # sudo sh -c "jq --tab '.process.terminal|=false' runc_workspace/runc/bundle/config.json > runc_workspace/runc/bundle/config2.json"
 # sudo sh -c "cat runc_workspace/runc/bundle/config2.json | jq --tab . > runc_workspace/runc/bundle/config.json"
 # rm -f runc_workspace/runc/bundle/config2.json
-sudo ./runc run test-ubuntu -b runc_workspace/runc/bundle 
+sudo ./runc --debug run test-ubuntu -b runc_workspace/runc/bundle 
