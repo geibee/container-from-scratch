54f3b6b13570da8f6252d68d4260d951c6468954
regmarmcem <tknktknktknktknktknk@gmail.com>
2022-12-28 12:58:53 +0900

    [fix] test.sh

--- test.sh
+++ test.sh
@@ -1,15 +1,15 @@
 mkdir -p tmp
-rm tmp/exec.fifo > /dev/null 2>&1
-rm ./cfs > /dev/null 2>&1
-if ls rootfs > /dev/null 2>&1; then
-    echo "rootfs" exists
-else
-    docker run --rm -d --name ubuntu ubuntu:18.04 tail -f /dev/null
-    docker export ubuntu > rootfs.tar
-    tar xf rootfs.tar -C rootfs
-    rm rootfs.tar
-fi
+mkdir -p rootfs
+docker run --rm -d --name ubuntu ubuntu:18.04 tail -f /dev/null
+docker export ubuntu > rootfs.tar
+docker kill ubuntu
+docker container prune -f
+tar xf rootfs.tar -C rootfs
+rm rootfs.tar
+
 str="$(IFS=,; echo "${@}")"
 go build -o cfs -tags=linux
 sudo ./cfs run --command "$str"
 rm -f ./cfs
+rm -rf ./rootfs
+rm -f tmp/exec.fifo 
